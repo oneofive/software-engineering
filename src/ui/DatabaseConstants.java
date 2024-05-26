7a27a2d0b761dac839c110b1c51bc91d1b5ca98f
Manseo Han <103206061+oneofive@users.noreply.github.com>
2024-05-26 16:59:57 +0900

    Delete src directory

--- src/ui/DatabaseConstants.java
+++
@@ -1,7 +0,0 @@
-package ui;
-
-public class DatabaseConstants {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/goalBuddy";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "비밀번호"; // mySQL 비밀버호 입력
-}
